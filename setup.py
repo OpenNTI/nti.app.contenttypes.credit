b87c274c86d881d8d9d744f99184b5e486f17a6f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-04-30 12:42:18 -0500

    Initial check-in

---
+++ setup.py
@@ -0,0 +1,75 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages 
+
+entry_points = {
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
+}
+
+
+TESTS_REQUIRE = [
+    'nti.app.testing',
+    'nti.testing',
+    'zope.dottedname',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.app.contenttypes.credit',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Application Credit Content Types",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='pyramid content credit',
+    classifiers=[
+        'Framework :: Zope',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.app.contenttypes.credit",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app', 'nti.app.contenttypes'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'nti.contenttypes.credit',
+        'nti.externalization',
+        'nti.links',
+        'nti.schema',
+        'pyramid',
+        'zope.i18nmessageid',
+        'zope.location',
+        'zope.component',
+        'zope.interface',
+        'zope.security',
+        'zope.traversing',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points
+)
