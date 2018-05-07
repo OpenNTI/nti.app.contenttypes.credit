b2c0fadf680531c3afde360f51676c70cf210ef8
Josh Zuech <josh.zuech@nextthought.com>
2018-05-06 23:20:24 -0500

    Add migration to install credit def container in each site registry

--- src/nti/app/contenttypes/credit/interfaces.py
+++ src/nti/app/contenttypes/credit/interfaces.py
@@ -9,3 +9,26 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 # pylint: disable=inherit-non-class
+
+from zope import interface
+
+from zope.container.constraints import contains
+from zope.container.constraints import containers
+
+from zope.container.interfaces import IContained
+from zope.container.interfaces import IContainer
+
+from nti.base.interfaces import ICreated
+from nti.base.interfaces import ILastModified
+
+from nti.contenttypes.credit.interfaces import ICreditDefinition
+
+from nti.ntiids.schema import ValidNTIID
+
+from nti.schema.field import Number
+from nti.schema.field import Object
+from nti.schema.field import DecodingValidTextLine as ValidTextLine
+
+
+class IUserAwardedCreditTranscript(interface.Interface):
+    pass
--- src/nti/app/contenttypes/credit/tests/__init__.py
+++ src/nti/app/contenttypes/credit/tests/__init__.py
@@ -5,4 +5,41 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-# pylint: disable=protected-access,too-many-public-methods,arguments-differ
+from persistent import Persistent
+
+from zope.component.persistentregistry import PersistentComponents as Components
+
+import zope.testing.cleanup
+
+from nti.testing.layers import ZopeComponentLayer
+from nti.testing.layers import ConfiguringLayerMixin
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+
+class SharedConfiguringTestLayer(ZopeComponentLayer,
+                                 ConfiguringLayerMixin):
+
+    set_up_packages = ('nti.app.contenttypes.credit',)
+
+    @classmethod
+    def setUp(cls):
+        cls.setUpPackages()
+
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        zope.testing.cleanup.cleanUp()
+
+    @classmethod
+    def testSetUp(cls):
+        pass
+
+    @classmethod
+    def testTearDown(cls):
+        pass
+
+
+class CreditLayerTest(ApplicationLayerTest):
+
+    layer = SharedConfiguringTestLayer
